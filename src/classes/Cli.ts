8df73bc2fafb6aa59b47f53ce192fdbc8fd6f386
mbaker <mekenna.drabner@gmail.com>
2024-08-04 12:29:36 -0600

    declared truck properties, and created constructor. Completed answers object

--- src/classes/Cli.ts
+++ src/classes/Cli.ts
@@ -174,10 +174,25 @@ class Cli {
         },
       ])
       .then((answers) => {
-        // TODO: Use the answers object to pass the required properties to the Truck constructor
+        const truck = new Truck(
+          Cli.generateVin(),
+          answers.color,
+          answers.make,
+          answers.model,
+          parseInt(answers.year),
+          parseInt(answers.weight),
+          parseInt(answers.topSpeed),
+          [],
+          parseInt(answers.towingCapacity),
+        );
+        
+        // TODO: Use the answers object to pass the required properties to the Truck constructor //
         // TODO: push the truck to the vehicles array
-        // TODO: set the selectedVehicleVin to the vin of the truck
-        // TODO: perform actions on the truck
+        this.vehicles.push(truck);
+        // TODO: set the selectedVehicleVin to the vin of the truck //
+        this.selectedVehicleVin = truck.vin;
+        // TODO: perform actions on the truck //
+        this.performActions();
       });
   }
 
@@ -237,10 +252,16 @@ class Cli {
         },
       ])
       .then((answers) => {
+        const motorbike = new Motorbike(
+
+        );
         // TODO: Use the answers object to pass the required properties to the Motorbike constructor
-        // TODO: push the motorbike to the vehicles array
-        // TODO: set the selectedVehicleVin to the vin of the motorbike
+        // TODO: push the motorbike to the vehicles array//
+        this.vehicles.push(motorbike);
+        // TODO: set the selectedVehicleVin to the vin of the motorbike //
+         this.selectedVehicleVin = motorbike.vin;
         // TODO: perform actions on the motorbike
+        this.performActions();
       });
   }
 
